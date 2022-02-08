a65d8fee17a58e0d05e3d1ddfd9778cac0d7e504
Sachyaaa <84377689+Sachyaaa@users.noreply.github.com>
2022-02-08 19:44:31 +0530

    functanality added

---
+++ claculator/index.js
@@ -0,0 +1,16 @@
+let input = document.getElementById('input');
+
+function display(n){
+    input.value += n;
+}
+function reset(){
+    input.value = '';
+}
+function del(){
+    let str = input.value;
+    input.value = str.substr(0,str.length-1);
+}
+function ans(){
+    let x = eval(input.value);
+    input.value = x;
+}
